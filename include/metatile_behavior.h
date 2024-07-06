ef2cca7798ee814b2c3ea497a44ea2c00ccee191
ashu <austinshuuu@gmail.com>
2024-07-06 15:48:04 -0700

    desert grass battle animations

--- include/metatile_behavior.h
+++ include/metatile_behavior.h
@@ -160,5 +160,6 @@ bool8 MetatileBehavior_IsSidewaysStairsRightSideAny(u8 metatileBehavior);
 bool8 MetatileBehavior_IsSidewaysStairsLeftSideAny(u8 metatileBehavior);
 bool8 MetatileBehavior_IsNonAnimDoorLeft(u8);
 bool8 MetatileBehavior_IsNonAnimDoorRight(u8);
+bool8 MetatileBehavior_IsSandGrass(u8);
 
 #endif // GUARD_METATILE_BEHAVIOR_H
