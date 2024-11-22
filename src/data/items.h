34bb268f521b095fd1205e0bb86dd662e37518a9
ashu <austinshuuu@gmail.com>
2024-11-22 15:04:11 -0800

    test crystal flute functionality fix

--- src/data/items.h
+++ src/data/items.h
@@ -9699,7 +9699,7 @@ const struct Item gItems[] =
         .holdEffectParam = 150,
         .description = sWhiteFluteDesc,
         .pocket = POCKET_ITEMS,
-        .type = ITEM_USE_PARTY_MENU,
+        .type = ITEM_USE_BAG_MENU,
         .fieldUseFunc = ItemUseOutOfBattle_CrystalFlute,
         .flingPower = 30,
     },
