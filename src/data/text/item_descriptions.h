c0abe3e57b946ee1002f4e8c95586a6f6062c532
ashu <austinshuuu@gmail.com>
2025-06-18 15:10:24 -0700

    Kushinada Gym

--- src/data/text/item_descriptions.h
+++ src/data/text/item_descriptions.h
@@ -3282,7 +3282,9 @@ static const u8 sTM50Desc[] = _(
     "lowers Sp. Atk.");
 
 static const u8 sTM51Desc[] = _( // Todo
-    "?????");
+    "The user damages\n"
+    "opponents, emitting\n"
+    "a powerful flash.");
 
 static const u8 sTM52Desc[] = _( // Todo
     "?????");
