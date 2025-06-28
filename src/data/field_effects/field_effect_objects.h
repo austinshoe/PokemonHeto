9e3659b0f2d6a540a94b8399555d7ad77b6ae025
ashu <austinshuuu@gmail.com>
2025-06-28 13:23:18 -0400

    OW sprite add - surf blob fix

--- src/data/field_effects/field_effect_objects.h
+++ src/data/field_effects/field_effect_objects.h
@@ -213,8 +213,8 @@ static const union AnimCmd *const sAnimTable_SurfBlob[] =
 
 const struct SpriteTemplate gFieldEffectObjectTemplate_SurfBlob = {
     .tileTag = TAG_NONE,
-    //.paletteTag = TAG_NONE,
-    .paletteTag = OBJ_EVENT_PAL_TAG_NEW_NPC_2,
+    .paletteTag = TAG_NONE,
+    //.paletteTag = OBJ_EVENT_PAL_TAG_NEW_NPC_2,
     .oam = &gObjectEventBaseOam_32x32,
     .anims = sAnimTable_SurfBlob,
     .images = sPicTable_SurfBlob,
